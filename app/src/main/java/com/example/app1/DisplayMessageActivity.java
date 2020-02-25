5c49872b72ada498459968ec029a684bf6d576df
kirshna2095 <sk7029@outlook.com>
2020-02-25 12:35:57 +0530

    my first android app

---
+++ app/src/main/java/com/example/app1/DisplayMessageActivity.java
@@ -0,0 +1,19 @@
+package com.example.app1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.app1_MESSAGE);
+        TextView textView = findViewById(R.id.textView2);
+        textView.setText(message);
+    }
+}
